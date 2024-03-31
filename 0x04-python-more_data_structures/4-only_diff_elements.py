ddadb73239913f82a84644a9694299dbb7d3342a
Trevor Gituru <giturutrevor@gmail.com>
2024-03-31 22:32:06 +0300

    a function that returns a set of elements present in only one set

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    """
+    a function that returns a set of all elements present in only one set
+    """
+    if set_1 is not None and set_2 is not None:
+        return set_1.symmetric_difference(set_2)

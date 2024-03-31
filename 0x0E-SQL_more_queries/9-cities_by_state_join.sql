506326f93ccda738b73ef57aa21af24ef09b9356
Trevor Gituru <giturutrevor@gmail.com>
2024-03-31 23:38:19 +0300

    implicit join with one select statement

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- lists all cities contained in the database hbtn_0d_usa.
 -- Each record should display: cities.id - cities.name - states.name
 -- Results must be sorted in ascending order by cities.id
--- You can use only one SELECT statement
+--- You can use only one SELECT statement
 
 SELECT cities.id, cities.name, states.name
 FROM cities, states

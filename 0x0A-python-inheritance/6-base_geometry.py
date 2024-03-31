72e6eaa12f009b7e707bb9b0e1961f08a79ef164
Trevor Gituru <giturutrevor@gmail.com>
2024-03-31 23:00:41 +0300

    def area(self): that raises an Exception with the message area() is not implemente

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+contains the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """a class"""
+    def area(self):
+        """empty method"""
+        raise Exception('area() is not implemented')

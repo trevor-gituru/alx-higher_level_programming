f474571f352e616f45d0912dafdb176c7ff95dab
Trevor Gituru <giturutrevor@gmail.com>
2024-03-31 23:22:15 +0300

    state by name searched

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+state object with name passed as argument
+parameters: username, password, database name, searched state
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db = argv[3]
+    searched_state = argv[4]
+
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}\
+                           @localhost:3306/{db}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == searched_state).first()
+    if state:
+        print(state.id)
+    else:
+        print('Not found')
+
+    session.close()

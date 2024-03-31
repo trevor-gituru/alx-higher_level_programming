310f91a6df45606aab89a82acf298a2392619783
Trevor Gituru <giturutrevor@gmail.com>
2024-04-01 00:00:23 +0300

    a class Student that defines a student by: (based on 10-student.py)

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+contains the class Student
+"""
+
+
+class Student:
+    """defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dicionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        return {key: value for (key, value) in self.__dict__.items()
+                if key in attrs}
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)

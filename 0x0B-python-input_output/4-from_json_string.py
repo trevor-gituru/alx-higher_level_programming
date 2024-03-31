77b11b2db3cd45e52047090e1f7a3fbce6926b31
Trevor Gituru <giturutrevor@gmail.com>
2024-04-01 00:01:44 +0300

    a function that returns an object (Python data structure) represented by JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+contains from_json_string function
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    returns an object represented by a JSON string:
+    """
+    return json.loads(my_str)

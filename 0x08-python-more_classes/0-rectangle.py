3b5dda19f54bac9c5967527997deb9c49793193f
Trevor Gituru <giturutrevor@gmail.com>
2024-03-31 22:45:16 +0300

    task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module: 0-rectangle
+contains the class Rectangle
+the class Rectangle is an empty class
+"""
+
+
+class Rectangle:
+    """
+    Defines a rectangle
+    """
+    pass

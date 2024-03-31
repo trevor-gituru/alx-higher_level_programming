41e5bcd5e01a309c5ca6cf2e487e68f45b413995
Trevor Gituru <giturutrevor@gmail.com>
2024-03-31 23:50:45 +0300

    task 20

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python3
+"""
+model: base
+"""
+
+import json
+import csv
+
+
+class Base:
+    """Base  of all other classes in the project"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            self.__class__.__nb_objects += 1
+            self.id = self.__class__.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        if list_objs is None:
+            list_objs = []
+        json_list = [obj.to_dictionary() for obj in list_objs]
+        filename = cls.__name__ + '.json'
+        with open(filename, 'w', encoding='utf-8') as json_file:
+            json_file.write(cls.to_json_string(json_list))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(1, 1)
+        if cls.__name__ == "Square":
+            dummy_instance = cls(1)
+        dummy_instance.update(**dictionary)
+        return dummy_instance
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        filename = cls.__name__ + ".json"
+        li = []
+        try:
+            with open(filename, 'r', encoding='utf-8') as file:
+                instances = cls.from_json_string(file.read())
+            for i, dic in enumerate(instances):
+                li.append(cls.create(**instances[i]))
+        except Exception:
+            pass
+        return li
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """serializes in CSV"""
+        filename = cls.__name__ + ".csv"
+        with open(filename, 'w', newline='') as f:
+            writer = csv.writer(f)
+            for o in list_objs:
+                if cls.__name__ == "Rectangle":
+                    writer.writerow([o.id, o.width, o.height, o.x, o.y])
+                if cls.__name__ == "Square":
+                    writer.writerow([o.id, o.size, o.x, o.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """deserializes in CSV"""
+        objs = []
+        filename = cls.__name__ + ".csv"
+        with open(filename, 'r', newline='') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if cls.__name__ == "Rectangle":
+                    dic = {"id": int(row[0]),
+                           "width": int(row[1]),
+                           "height": int(row[2]),
+                           "x": int(row[3]),
+                           "y": int(row[4])}
+                if cls.__name__ == "Square":
+                    dic = {"id": int(row[0]),
+                           "size": int(row[1]),
+                           "x": int(row[2]),
+                           "y": int(row[3])}
+                o = cls.create(**dic)
+                objs.append(o)
+        return objs

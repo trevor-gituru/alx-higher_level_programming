d47ae58ee24c6943baba532f3aba3d7da051f02e
Trevor Gituru <giturutrevor@gmail.com>
2024-03-31 22:53:42 +0300

    class Ssssssquare that inherits from Rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+contains class square
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """derived class"""
+    def __init__(self, size):
+        """constructor"""
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size

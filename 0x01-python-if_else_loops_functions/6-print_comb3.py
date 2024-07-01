418c5a6342af085a0bcd50768cc75c25d3b11e28
Trevor Gituru <giturutrevor@gmail.com>
2024-07-01 14:24:19 +0300

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 9):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
-            print("{:d}{:d}".format(i, j))
+            print(f"{i:d}{j:d}")
         else:
             print("{:d}{:d}".format(i, j), end=", ")

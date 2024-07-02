1cae4e785474e0d85f2d725a2fef48adae6dfd6d
Trevor Gituru <giturutrevor@gmail.com>
2024-07-02 15:24:21 +0300

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
 
-    names_list = dir(hidden_4)
-    for name in names_list:
+    names_lists = dir(hidden_4)
+    for name in names_lists:
         if name[0:2] != "__":
             print(name)

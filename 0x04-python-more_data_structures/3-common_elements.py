63d41b310bdf41665c79b9a1637bdc3a42a2e016
Trevor Gituru <giturutrevor@gmail.com>
2024-07-03 16:41:13 +0300

    Task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -4,4 +4,4 @@ def common_elements(set_1, set_2):
     a function that returns a set of common elements in two sets
     """
     if set_1 is not None and set_2 is not None:
-        return set_1.intersection(set_2)
+        return set_1 & set_2

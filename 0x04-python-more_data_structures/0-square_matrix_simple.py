3e8a69c1df04b0ebd752398985e76d6551a28afc
Trevor Gituru <giturutrevor@gmail.com>
2024-07-03 16:22:40 +0300

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,9 +4,4 @@ def square_matrix_simple(matrix=[]):
     a function that computes the square value of all integers in a matrix
     """
     if matrix is not None:
-        square_matrix = []
-        for row in matrix:
-            square_values = map(lambda x: x ** 2, row)
-            squared_list = list(square_values)
-            square_matrix.append(squared_list)
-        return square_matrix
+        return list(map(lambda row: list(map(lambda element: element**2, row)) , matrix))

57c7fdc3855af7ffe8f646bf9738a09cd90ffcad
Trevor Gituru <giturutrevor@gmail.com>
2024-07-03 16:49:28 +0300

    Task 5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -4,7 +4,4 @@ def number_keys(a_dictionary):
     a function that returns the number of keys in a dictionary
     """
     if a_dictionary is not None:
-        count = 0
-        for key in a_dictionary:
-            count += 1
-        return count
+        return len(a_dictionary)

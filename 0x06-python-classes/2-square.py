f9e2b0c04e35d75f23059c5c0c7299b899bc0edf
Trevor Gituru <giturutrevor@gmail.com>
2024-07-05 13:42:33 +0300

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-module: 2-square
-defines a class Square
+Module: 2-square
+Defines a class Square.
 """
 
 
 class Square:
     """defines a square"""
     def __init__(self, size=0):
-        self.__size = size
-
-        if type(self.__size) is not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
 
-        if self.__size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
+

4536b5e4b977a6bcf39e1c9c702344610a11e8ea
Trevor Gituru <giturutrevor@gmail.com>
2024-07-06 12:25:25 +0300

    Added Task 3 description

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 """
-module: 0-rectangle
-contains the class Rectangle
+Module: 0-rectangle
+Contains the class Rectangle
 Rectangle has the attributes width and height
 """
 
 
 class Rectangle:
     """
-    defines a rectangle
+    Defines a rectangle
     """
     def __init__(self, width=0, height=0):
-        """initializes the rectangles fields"""
+        """Initializes the rectangles fields"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """getter that retrives the width"""
+        """Getter that retrives the width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """setter that sets width"""
+        """Setter that sets width"""
         if type(value) is not int:
             raise TypeError('width must be an integer')
 
@@ -33,12 +33,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """getter that retrives the height"""
+        """Getter that retrives the height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """setter that sets height"""
+        """Setter that sets height"""
         if type(value) is not int:
             raise TypeError('height must be an integer')
 
@@ -48,23 +48,21 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """returns rectangle area"""
+        """Returns rectangle area"""
         return (self.__height * self.__width)
 
     def perimeter(self):
-        """returns rectangle perimeter"""
+        """Returns rectangle perimeter"""
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return (2 * (self.__height + self.__width))
 
     def __str__(self):
-        """prints the rectanglw with #s"""
+        """Prints the rectanglw with #s"""
         if self.__height == 0 or self.__width == 0:
             return ""
         rect_str = ''
-        for i in range(self.__height):
-            for j in range(self.__width):
-                rect_str += '#'
-            rect_str += '\n'
+        for _ in range(self.__height):
+            rect_str += '#' * self.__width + '\n'
         return rect_str[:-1]

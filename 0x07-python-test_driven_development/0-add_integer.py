5d8a6a5026cd9628dc62512800641cc9b6de8c18
Trevor Gituru <152320650+trevor-gituru@users.noreply.github.com>
2024-07-11 14:14:25 +0300

    Task 0: Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-"""contains a methos that adds 2 integers
-    Args: a = first value
-          b = second value
-    return: an integer: the addition of a and b
-    raise: TypeError exception if a or b is not int or float"""
+"""Defines an integer addition function."""
 
 
 def add_integer(a, b=98):
+    """Return the integer addition of a and b.
+
+    Float arguments are typecasted to ints before addition is performed.
+
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-    returns the integer sum of values a and b
-    """
-    if isinstance(a, float):
-        a = int(a)
-
-    if isinstance(b, float):
-        b = int(b)
-
-    if not isinstance(a, (int, float)):
-        raise TypeError('a must be an integer')
-
-    if not isinstance(b, (int, float)):
-        raise TypeError('b must be an integer')
-
-    return a + b
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))

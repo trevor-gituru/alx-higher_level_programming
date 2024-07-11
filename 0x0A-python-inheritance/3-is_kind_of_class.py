9da60e81a7cd98cae027e87a04ddd94543db4b8f
Trevor Gituru <giturutrevor@gmail.com>
2024-07-11 22:06:59 +0300

    Task 3: Same class or inherit from

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,5 +5,5 @@ contains is_kind_of_class
 
 
 def is_kind_of_class(obj, a_class):
-    """checks for an instance of an object"""
+    """Checks for an instance of an object"""
     return isinstance(obj, a_class)

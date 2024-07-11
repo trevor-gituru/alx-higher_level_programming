7a3d505e6b4d749be13597282e521cbca0754bec
Trevor Gituru <giturutrevor@gmail.com>
2024-07-11 22:03:06 +0300

    Task 2: Exact same object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,5 +5,5 @@ contains the method is_same_class
 
 
 def is_same_class(obj, a_class):
-    """checks for an instance of an object"""
+    """Checks for an instance of an object"""
     return type(obj) is a_class

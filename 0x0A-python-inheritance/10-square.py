6fee87e38b58a46361e6cce0febc9287666ffbdf
Trevor Gituru <giturutrevor@gmail.com>
2024-07-12 12:07:46 +0300

    Task 10: Square #1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""
-contains class square
-"""
-
-
+"""Defines a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """derived class"""
+    """Represent a square."""
+
     def __init__(self, size):
-        """constructor"""
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size

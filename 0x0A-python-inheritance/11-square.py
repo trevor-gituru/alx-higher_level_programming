77a44d3e52b80372a6c40f1919d9cc9f2e118ae8
Trevor Gituru <giturutrevor@gmail.com>
2024-07-12 12:14:13 +0300

    Task 11: Square #2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-contains class square
+Contains a Rectangle subclass, class square
 """
 
 
@@ -8,9 +8,13 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """derived class"""
+    """Represent a square"""
     def __init__(self, size):
-        """constructor"""
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size

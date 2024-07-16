2c838a0538d097fba3a911f762e1bc67623810e7
Trevor Gituru <giturutrevor@gmail.com>
2024-07-16 13:13:53 +0300

    Task 2: Append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """
-contains the append_write function
+Contains the append_write function
 """
 
 
 def append_write(filename="", text=""):
     """
-    appends a string at the end of a text file (UTF8)
+    Appends a string at the end of a text file (UTF8)
+    Args:
+        filename:   The name of file
+        text:   The string to append to file
     return:
-            the number of characters added:
+        the number of characters added:
     """
     with open(filename, 'a', encoding='utf-8') as file:
         return file.write(text)

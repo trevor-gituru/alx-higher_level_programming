99a8f6e983a7adc7e13e4a76b083e7c7d69508ba
Trevor Gituru <giturutrevor@gmail.com>
2024-07-16 16:36:56 +0300

    Task 9: Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """
-contains the class Student
+Contains the class Student
 """
 
 
 class Student:
-    """defines a student"""
+    """Defines a student"""
     def __init__(self, first_name, last_name, age):
-        """constructor"""
+        """Constructor"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """dicionary representation of a Student instance"""
+        """Dicionary representation of a Student instance"""
         return {key: value for (key, value) in self.__dict__.items()
                 if key in list(self.__dict__.keys())}

4a78be340dc28c73f74fa8e1df816f8d32e6ee4f
Trevor Gituru <giturutrevor@gmail.com>
2024-07-16 16:28:41 +0300

    Task 8: Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,6 +5,6 @@ contains the function class_to_json
 
 
 def class_to_json(obj):
-    """returns the dictionary description with simple data structure"""
+    """Returns the dictionary description with simple data structure"""
     return {key: value for (key, value) in obj.__dict__.items()
             if key in list(obj.__dict__.keys())}

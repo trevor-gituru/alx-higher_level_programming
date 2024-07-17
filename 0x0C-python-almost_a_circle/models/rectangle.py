bfcd1bdea8c26e47ad43ca0c36be65495b7b844e
Trevor Gituru <giturutrevor@gmail.com>
2024-07-17 17:17:04 +0300

    Task 5: Display #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,14 +95,8 @@ class Rectangle(Base):
 
     def display(self):
         """prints the Rectangle instance with the character #"""
-        for _ in range(self.__y):
-            # vertical offset - y-coordinate
-            print()
-
-        for _ in range(self.__height):
-            # horizontal offset - x-coordinate
-            print((" " * self.__x) + ("#" * self.__width))
-
+        for _ in range(self.height):
+            print('#' * self.width)
     def __str__(self):
         """returns string representation of an obeject"""
         return "[{:s}] ({:d}) {:d}/{:d} - {:d}/{:d}".format(

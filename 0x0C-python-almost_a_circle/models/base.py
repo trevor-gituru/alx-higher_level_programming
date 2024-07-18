e5f76ea94142c748dc0b6821634819ab28e48517
Trevor Gituru <giturutrevor@gmail.com>
2024-07-18 14:35:38 +0300

    Task 18: Dictionary to Instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -47,12 +47,9 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """returns an instance with all attributes already set"""
-        if cls.__name__ == "Rectangle":
-            dummy_instance = cls(1, 1)
-        if cls.__name__ == "Square":
-            dummy_instance = cls(1)
-        dummy_instance.update(**dictionary)
-        return dummy_instance
+        dummy = cls(1, 1)
+        dummy.update(**dictionary)
+        return dummy
 
     @classmethod
     def load_from_file(cls):

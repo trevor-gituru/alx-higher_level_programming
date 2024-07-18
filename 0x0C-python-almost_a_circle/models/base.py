bc9fc61fc339709f57e6ee73ad70f3c9051ab52f
Trevor Gituru <giturutrevor@gmail.com>
2024-07-18 14:14:47 +0300

    Task 17: JSON string to dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,7 +28,7 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        """returns the list of the JSON string representation json_string"""
+        """Returns the list of the JSON string representation json_string"""
         if json_string is None or len(json_string) == 0:
             return []
         else:

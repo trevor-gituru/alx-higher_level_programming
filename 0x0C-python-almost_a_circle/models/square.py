30bd36e31514bfbc1d733a51790909cb0500bc9d
Trevor Gituru <giturutrevor@gmail.com>
2024-07-18 12:37:23 +0300

    Task 9: Update #1

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,7 +12,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """class contructor"""
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     @property
     def size(self):

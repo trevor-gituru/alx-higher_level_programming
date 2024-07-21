910ed91921594a7b856b6a6d284c487fcde1bc4d
Trevor Gituru <giturutrevor@gmail.com>
2024-07-21 16:37:43 +0300

    Task 9: Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- crete table and add multiple rows
+-- Crete table second_table and add multiple rows
 
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS second_table (
     score INT
 );
 
-INSERT INTO second_table (id, name, score) VALUE(1, 'John', 10);
-INSERT INTO second_table (id, name, score) VALUE(2, 'Alex', 3);
-INSERT INTO second_table (id, name, score) VALUE(3, 'Bob', 14);
-INSERT INTO second_table (id, name, score) VALUE(4, 'George', 8);
+INSERT INTO second_table (id, name, score) VALUES(1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES(2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES(3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES(4, 'George', 8);

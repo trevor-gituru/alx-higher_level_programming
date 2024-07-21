0a9fa4ffe8d890539b97c9af0513b6ac068c9dcd
Trevor Gituru <giturutrevor@gmail.com>
2024-07-21 16:49:51 +0300

    Task 12: Cheating is bad

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,5 @@
--- updates the score of Bob to 10
--- not allowed to use Bob’s id value, only name field
+-- Updates the score of Bob to 10
+-- Not allowed to use Bob’s id value, only name field
 
 UPDATE second_table
 SET score=10

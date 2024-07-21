ff2f6ac4e4fa8bb11eaa89ca6a6cc45d6c05e81b
Trevor Gituru <giturutrevor@gmail.com>
2024-07-21 16:46:14 +0300

    Task 11:

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
--- lists all records  with a score >= 10
+-- Lists all records  with a score >= 10
 -- Results should display both the score and the name (in this order)
 -- Records should be ordered by score (top first)
 

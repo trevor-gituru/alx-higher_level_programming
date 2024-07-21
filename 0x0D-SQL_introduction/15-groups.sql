8489f3b62c6dfe821aa4a9c0c0b3ccdfa266d0fc
Trevor Gituru <giturutrevor@gmail.com>
2024-07-21 17:03:36 +0300

    Task 15: Number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,7 +1,7 @@
--- lists the number of records with the same score
+-- Lists the number of records with the same score
 -- The result should display:
--- 1.the score
--- 2.the number of records for this score with the label number
+--      1.the score
+--      2.the number of records for this score with the label number
 -- The list should be sorted by the number of records (descending)
 
 SELECT score, COUNT(*) AS number

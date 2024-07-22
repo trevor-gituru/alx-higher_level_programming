5e7d4d611fdcd412332a45d3b01ddf5845673903
Trevor Gituru <giturutrevor@gmail.com>
2024-07-22 17:58:11 +0300

    Task 15: Only Comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -6,10 +6,10 @@
 -- You can use only one SELECT statement
 
 SELECT tv_shows.title
-FROM tv_shows
+    FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
+    ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
+    ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = "Comedy"
 ORDER BY tv_shows.title ASC;

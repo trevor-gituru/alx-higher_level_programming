a511837a2592b1d97a2c3073e1b0e007b0e9bc17
Trevor Gituru <giturutrevor@gmail.com>
2024-07-22 16:55:44 +0300

    Task 10: Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,7 +5,7 @@
 -- You can use only one SELECT statement
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
+    FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+    ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;

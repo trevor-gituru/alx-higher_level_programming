6fd5eba62f96d25d76f62b4843f683d4d0d0f4b4
Trevor Gituru <giturutrevor@gmail.com>
2024-07-22 18:09:43 +0300

    Task 16: List shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -6,9 +6,9 @@
 -- You can use only one SELECT statement
 
 SELECT tv_shows.title, tv_genres.name
-FROM tv_shows
+    FROM tv_shows
 LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
+    ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
+    ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title, tv_genres.name ASC;

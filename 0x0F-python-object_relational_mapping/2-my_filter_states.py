9203133efd51cc536f8749654714b8faabac6c02
Trevor Gituru <giturutrevor@gmail.com>
2024-07-24 16:31:53 +0300

    Task 2: Filter states by user input python script

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,10 +15,14 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states\
-                WHERE name LIKE '{}' ORDER BY id ASC".format(argv[4]))
+    cur.execute(
+        "SELECT * FROM states\
+        WHERE name LIKE '{}'\
+        ORDER BY id ASC".format(argv[4])
+    )
     rows = cur.fetchall()
     for row in rows:
+        # To account for case insentivity search
         if row[1] == argv[4]:
             print(row)
     cur.close()

af1d6bbe7a798a607a68223de0617c412fc3307a
Trevor Gituru <giturutrevor@gmail.com>
2024-07-24 21:35:21 +0300

    Task 12: Update a state python script

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -6,23 +6,28 @@ parameters: username, password, database name
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine.url import URL
 from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-    username = argv[1]
-    password = argv[2]
-    db = argv[3]
+    mysql_db = {
+        'drivername': 'mysql',
+        'username': argv[1],
+        'password': argv[2],
+        'database': argv[3],
+        'host': 'localhost',
+        'port': 3306
+    }
 
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}\
-                           @localhost:3306/{db}')
+    engine = create_engine(URL.create(**mysql_db))
     Session = sessionmaker(bind=engine)
     session = Session()
 
     # update state object
-    session.query(State).filter(State.id == 2).update(
-            {"name": "New Mexico"}, synchronize_session='fetch'
-            )
+    stat_id = 2
+    state1 = session.get(State, stat_id)
+    state1.name = 'New Mexico'
     session.commit()
 
     session.close

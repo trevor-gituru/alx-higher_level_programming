080134570daf01150c635ceee56e5eb523de6e63
Trevor Gituru <giturutrevor@gmail.com>
2024-07-24 22:33:53 +0300

    Task 14: Cities in state python script

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -7,15 +7,14 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base, State
 from sqlalchemy.orm import relationship
 
+
 class City(Base):
     """City class"""
 
     __tablename__ = 'cities'
-    id = Column(Integer, autoincrement=True, 
+    id = Column(Integer, autoincrement=True,
                 nullable=False,
                 unique=True, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-    
     state = relationship(State, back_populates='cities')
-

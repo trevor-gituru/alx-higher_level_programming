2e5e2dc8a75b29f1640c069f7b271644803d5d3e
Trevor Gituru <giturutrevor@gmail.com>
2024-08-22 14:20:14 +0300

    Task 0: What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+0-hbtn_status.py
+
+Task 0
+
+This script fetches the URL https://alx-intranet.hbtn.io/status using
+the urllib package and displays the response body in a specified format.
+
+For detailed task description, requirements, and usage,
+refer to the README file.
+"""
+from urllib.request import urlopen
+
+url = 'https://alx-intranet.hbtn.io/status'
+with urlopen(url) as response:
+    res_headers = response.info()
+    content = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode('utf8')))

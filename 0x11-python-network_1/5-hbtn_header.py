59acc2d4ac3127b9ee2ed9539fbb6b3908cbffe9
Trevor Gituru <giturutrevor@gmail.com>
2024-08-22 17:32:00 +0300

    Task 5: Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""
+5-hbtn_header.py
+
+Task 5
+
+This script takes a URL as a command-line argument, sends a request to the
+URL, and displays the value of the `X-Request-Id` variable found in the
+response header.
+
+For detailed task description and requirements, refer to the README file.
+"""
 if __name__ == "__main__":
     import requests
     from sys import argv

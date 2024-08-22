d20d7b5ba161197392362e25b04b844c0b0de2ca
Trevor Gituru <giturutrevor@gmail.com>
2024-08-22 15:51:41 +0300

    Task 7: Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""
+7-error_code.py
+
+Task 0
+
+This script takes a URL as a command-line argument, sends a request to the
+URL, and displays the body of the response. If the HTTP status code is 400
+or greater, it prints: Error code: followed by the HTTP status code.
+
+For detailed task description and requirements, refer to the README file.
+"""
 if __name__ == "__main__":
     import requests
     from sys import argv

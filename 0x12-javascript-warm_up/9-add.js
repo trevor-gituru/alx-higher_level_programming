ce903ce878167aa8f2be6d6e6c51fe44c0ffd9b4
Trevor Gituru <giturutrevor@gmail.com>
2024-08-28 15:26:54 +0300

    Task 9: Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,5 +4,5 @@ function add (a, b) {
   return (a + b);
 }
 
-let sum = add(parseInt(process.argv[2]), parseInt(process.argv[3]));
+const sum = add(parseInt(process.argv[2]), parseInt(process.argv[3]));
 console.log(sum);

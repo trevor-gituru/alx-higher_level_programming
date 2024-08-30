acc557f74d5f88aec3af4275980383338dd93947
Trevor Gituru <giturutrevor@gmail.com>
2024-08-30 14:23:28 +0300

    Task 5: Square #0

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-
+// Task 5
 const Rectangle = require('./4-rectangle');
 
-module.exports = class Square extends Rectangle {
+class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-};
+}
+
+module.exports = Square;

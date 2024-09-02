ae79acddaaaa24de900526ebe2d0da4ce1cf85f5
Trevor Gituru <giturutrevor@gmail.com>
2024-09-02 13:50:38 +0300

    Task 0: Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Task 0
+
+const fs = require('fs');
+const file = process.argv[2];
+fs.readFile(file, function (err, data) {
+    if (err) {
+        return console.error(err);
+    }
+    console.log(data.toString());
+})

6523d679781f9fbc292bc4b2a4bcf1c15c32c144
Trevor Gituru <giturutrevor@gmail.com>
2024-09-02 13:53:12 +0300

    Task 0: Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,8 +4,8 @@
 const fs = require('fs');
 const file = process.argv[2];
 fs.readFile(file, function (err, data) {
-    if (err) {
-        return console.error(err);
-    }
-    console.log(data.toString());
-})
+  if (err) {
+    return console.error(err);
+  }
+  console.log(data.toString());
+});

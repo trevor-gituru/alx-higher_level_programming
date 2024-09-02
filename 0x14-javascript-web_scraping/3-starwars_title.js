58da84449832d4896b565bd4411404f1f3594fd6
Trevor Gituru <giturutrevor@gmail.com>
2024-09-02 14:45:08 +0300

    Task 3: Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Task 3
+
+const request = require('request');
+const requestURL = "https://swapi-api.alx-tools.com";
+const requestURI = "/api/films/" + process.argv[2];
+const fullURL = requestURL + requestURI;
+request(fullURL, function (err, response) {
+  if (err) {
+    return console.log(err);
+  }
+  if (response) {
+    console.log(JSON.parse(response.body).title);
+  }
+});
